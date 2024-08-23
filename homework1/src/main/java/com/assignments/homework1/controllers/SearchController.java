154fc881e8e67ae396e69eef1ba326b1ef75a3a7
Rudra Gujarathi <rudra.gujarathi123@gmail.com>
2024-08-22 18:18:23 -0700

    hw1 added

---
+++ homework1/src/main/java/com/assignments/homework1/controllers/SearchController.java
@@ -0,0 +1,36 @@
+package com.assignments.homework1.controllers;
+
+import com.assignments.homework1.models.University;
+import com.assignments.homework1.services.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SearchController {
+
+    private SearchService searchService;
+
+    @Autowired
+    public SearchController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    // http://localhost:8080/search/all
+    @GetMapping("/search/all")
+    public University[] search() {
+            return searchService.searchAll();
+    }
+
+    // http://localhost:8080/search/countries?country=United+States&country=China&country=India
+    @GetMapping("/search/countries")
+    public University[] search(@RequestParam(value = "country", required = false) List<String> countries) {
+        if(countries == null) {
+            return new University[] {};
+        }
+        return searchService.searchCountries(countries);
+    }
+}
---
+++ homework1/src/main/java/com/assignments/homework1/models/University.java
@@ -0,0 +1,12 @@
+package com.assignments.homework1.models;
+
+import java.util.List;
+
+public class University {
+    public List<String> web_pages;
+    public List<String> domains;
+    public String state_province;
+    public String country;
+    public String alpha_two_code;
+    public String name;
+}
